4ab38f57ed495d327458cabe0b84da5852b42bc6
StarylaptopDOM <aedd.istredd@gmail.com>
2017-10-21 14:44:46 +0200

    Working od sprawko

--- macheps.jl
+++ macheps.jl
@@ -1,42 +1,58 @@
 #!/usr/bin/env julia
+#=
+  @author: Bartosz Banasik
+=#
 
+# funkcja wyznaczająca iteracyjnie epsilon maszynowy
 function macheps()
+
+    # zmienne a , b, c są potrzebne do obliczenia 'macheps' odpowiednio
+    # dla danego typu
+
     a = Float64(1)
     b = Float32(1)
     c = Float16(1)
-    for T in [Int8, Int16, Int32, Int64, Int128, UInt8, UInt16, UInt32, UInt64, UInt128, UInt]
-        println("$(lpad(T,7)): [$(typemin(T)),$(typemax(T))]")
+
+    # pętla wyświetlająca dane patrzebne do porównań
+    for T in [Float64, Float32, Float16]
+        println("$(lpad(T,7)): [ eps: $(eps(T))]")
     end
 
-    for T in [Float16, Float32, Float64]
-        println("$(lpad(T,7)): [$(eps(T)), $(nextfloat(T(0.0)))")
-        println("$(lpad(T,7)): [$(realmin(T)),$(realmax(T))]")
-    end
-
-    while 1 + a > 1
+    # kolejne pętle iteracyjnie zmiejszają odpowiednie wartości
+    # do zadanego warunku ~(1 + x > 1) na końcu liczba x jest
+    # mnożona przez 2 ponieważ szukamy najmniejszej liczby spełniającej
+    # warunek
+    while 1.0 + a > 1.0
         a = a/2
     end
-    println(a*2)
+    println("Float 64: eps $(a*2)")
 
-    while 1 + b > 1
+    while Float32(1.0) + b > Float32(1.0)
         b = b/2
     end
-    println(b*2)
+    println("Float32: eps $(b*2)")
 
-    while 1 + c > 1
+    while Float16(1.0) + c > Float16(1.0)
         c = c/2
     end
-    println(c*2)
+    println("Float16 eps $(c*2)")
 
 end
 
-
-function jjjj()
-
+# funkcja wyznaczająca iteracyjnie wartość eta tzn.
+# eta > 0.0
+function etaNum()
+    # Zmienne na których będą wykonywane operacje
     a = Float64(1)
     b = Float32(1)
     c = Float16(1)
 
+    # pętla wyświetlająca dane do porównania
+    for T in [Float64, Float32, Float16]
+        println("$(lpad(T,7)): [ nextfloat: $(nextfloat(T(0.0)))]")
+    end
+
+    # kolejne pętle wyznaczają wartość eta dla odpowiednich typów
     while true
         if a/2>0
             a = a/2
@@ -44,7 +60,7 @@ function jjjj()
             break
         end
     end
-    println("$(typeof(a)): [$a, $(bits(a))]")
+    println("$(typeof(a)): eta [$a, $(bits(a))]")
 
     while true
         if b/2>0
@@ -53,7 +69,7 @@ function jjjj()
             break
         end
     end
-    println("$(typeof(b)): [$b, $(bits(b))]")
+    println("$(typeof(b)): eta [$b, $(bits(b))]")
 
 
     while true
@@ -63,11 +79,19 @@ function jjjj()
             break
         end
     end
-    println("$(typeof(c)): [$c, $(bits(c))")
+    println("$(typeof(c)): eta [$c, $(bits(c))")
 end
 
-
+# funckaj wyznaczająca iteracyjnie maksymalną wartość w danym typie
 function findmaximum()
+
+    # pętla wyświetlająca dane do porównania
+    for T in [Float64, Float32, Float16]
+        println("$(lpad(T,7)): [ MAX: $(realmax(T))]")
+    end
+
+    # pierwsza petla pozwala na 'utworzenie' makymalnej liczby
+    # przy niezmienionej cesze (maksymalna mantysa)
     a = Float64(1)
     b = Float64(1)
     while true
@@ -78,7 +102,9 @@ function findmaximum()
             break
         end
     end
-    bits(a)
+
+    # druga petla mnoży daną liczbę razy dwa
+    # tzn zwiększa jej cechę
     while true
         if !isinf(a*2)
             a = a * 2
@@ -86,11 +112,10 @@ function findmaximum()
             break
         end
     end
-    println(a," ",bits(a))
-    println(realmax(Float64)," ",bits(realmax(Float64)))
+    println("$(typeof(a)): max $(a)")
 
+    # kolejne petle tylko dla innych typów
     a = Float32(1)
-    println(bits(a))
     b = Float32(1)
     while true
         b = b/2
@@ -100,7 +125,7 @@ function findmaximum()
             break
         end
     end
-    println(bits(a))
+
     while true
         if !isinf(a*2)
             a = a * 2
@@ -108,8 +133,8 @@ function findmaximum()
             break
         end
     end
-    println(a," ",bits(a))
-    println(realmax(Float32)," ",bits(realmax(Float32)))
+    println("$(typeof(a)): max $a")
+
 
     a = Float16(1)
     b = Float16(1)
@@ -129,12 +154,11 @@ function findmaximum()
             break
         end
     end
-    println(a," ",bits(a))
-    println(realmax(Float16)," ",bits(realmax(Float16)))
+    println("$(typeof(a)): max $a")
 
 end
 
 
 #macheps()
-#jjjj()
+#etaNum()
 findmaximum()
